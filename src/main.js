59828ff89651ad3ce2c6b922dae347c22b4d52f7
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-12-09 08:57:45 +0800

    switch from CRA to vitre

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./css/Button.css";
 
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
@@ -14,28 +14,28 @@ import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import "react-tooltip/dist/react-tooltip.css";
 // import "primeflex/primeflex.css";
-import Home from "./pages/Home";
-import Flashcards from "./pages/Flashcards";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import Sets from "./pages/Sets";
-import NewSet from "./pages/NewSet";
-import Search from "./pages/Search";
-import Set from "./pages/Set";
-import Dictate from "./pages/Dictate";
-import Learn from "./pages/Learn";
+import Home from "./pages/Home.jsx";
+import Flashcards from "./pages/Flashcards.jsx";
+import Signup from "./pages/Signup.jsx";
+import Login from "./pages/Login.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Sets from "./pages/Sets.jsx";
+import NewSet from "./pages/NewSet.jsx";
+import Search from "./pages/Search.jsx";
+import Set from "./pages/Set.jsx";
+import Dictate from "./pages/Dictate.jsx";
+import Learn from "./pages/Learn.jsx";
 
 import { gsap, CSSPlugin } from "gsap";
 import { PixiPlugin } from "gsap/dist/PixiPlugin.js";
 import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin.js";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger.js";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./pages/helpers/ProtectedRoute";
-import MySets from "./pages/MySets";
+import Dashboard from "./pages/Dashboard.jsx";
+import ProtectedRoute from "./pages/helpers/ProtectedRoute.jsx";
+import MySets from "./pages/MySets.jsx";
 
 import "./theme.css";
-import EditSet from "./pages/EditSet";
+import EditSet from "./pages/EditSet.jsx";
 import TextPlugin from "gsap/dist/TextPlugin.js";
 
 gsap.registerPlugin(PixiPlugin, MotionPathPlugin, ScrollTrigger, TextPlugin);
